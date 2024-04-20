91b2b6d9ae2183cadaf5945f93cbd27d76bc8a25
Alfredo Aguilar <jaal0254@gmail.com>
2024-04-20 17:59:08 -0600

    add endpoint to verify token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import bcrypt from "bcrypt";
 import User from "../models/users.model.js";
 import { createAccessToken } from "../libs/jwt.js";
+import jwt from "jsonwebtoken";
+import { SECRET_TOKEN } from "../config.js";
 
 export const login = async (req, res) => {
   const { userName, password } = req.body;
@@ -31,4 +33,18 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
   res.cookie("token", "", { expires: new Date(0) });
   res.status(200).json({ message: "Logged out" });
+};
+
+export const verifyToken = async (req, res) => {
+  const { token } = req.cookies;
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  jwt.verify(token, SECRET_TOKEN, async (err, user) => {
+    if (err) return res.status(401).json({ message: "Unauthorized" });
+    const userFound = await User.findById(user.id);
+    if (!userFound) return res.status(401).json({ message: "Unauthorized" });
+    return res
+      .status(200)
+      .json({ userName: userFound.userName, message: "ok" });
+  });
 };
